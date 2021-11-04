1271990f2afb6652eeeac97edea549d1df89c524
vsrenato <avatarmestre999@gmail.com>
2021-11-04 17:25:55 -0300

    teste container

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Primeiro Pipeline!'));
+app.get('/', (req, res) => res.send('Teste container'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
